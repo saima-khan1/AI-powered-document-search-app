510caf3ed855aa5da378feb6d8420e7a4cbdf10f
Saima Sanjeeda Khan <119809514+saima-khan1@users.noreply.github.com>
2025-10-18 00:55:03 +0200

    fixed tailwind css issue

--- client/app/layout.tsx
+++ client/app/layout.tsx
@@ -1,15 +1,7 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
-
-import {
-  ClerkProvider,
-  SignInButton,
-  SignUpButton,
-  SignUp,
-  SignedIn,
-  SignedOut,
-  UserButton,
-} from "@clerk/nextjs";
+import "./globals.css";
+import { ClerkProvider, SignedIn, SignedOut, SignUp } from "@clerk/nextjs";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -28,26 +20,22 @@ export const metadata: Metadata = {
 
 export default function RootLayout({
   children,
-}: Readonly<{
+}: {
   children: React.ReactNode;
-}>) {
+}) {
   return (
     <ClerkProvider>
       <html lang="en">
         <body
-          className={`${geistSans.variable} ${geistMono.variable} antialiased`}
+          className={`${geistSans.variable} ${geistMono.variable} antialiased bg-[var(--background)] text-[var(--foreground)]`}
         >
-          <section className="w-screen min-h-screen flex items-center justify-center">
-            <SignedOut>
-              {" "}
+          <SignedOut>
+            <section className="w-screen min-h-screen flex items-center justify-center">
               <SignUp />
-            </SignedOut>
-          </section>
-          <SignedIn>
-            {" "}
-            {/* <UserButton /> */}
-            {children}
-          </SignedIn>
+            </section>
+          </SignedOut>
+
+          <SignedIn>{children}</SignedIn>
         </body>
       </html>
     </ClerkProvider>
