70e3a9f9c1512daa6674107e749f507c8bc2d12d
Saima Sanjeeda Khan <119809514+saima-khan1@users.noreply.github.com>
2025-10-22 08:38:47 +0200

    fixed undefined file name issue

--- server/index.ts
+++ server/index.ts
@@ -1,18 +1,18 @@
 import express from "express";
 import cors from "cors";
 import multer from "multer";
-// ✅ Simple disk storage setup that keeps original file name
+
 const storage = multer.diskStorage({
-  destination: "uploads/", // folder to save files
-  filename: (req, file, cb) => {
-    const uniqueSuffix = Date.now() + "-" + file.filename;
-    cb(null, uniqueSuffix);
+  destination: function (req, file, cb) {
+    cb(null, "uploads/");
+  },
+  filename: function (req, file, cb) {
+    const uniqueSuffix = Date.now() + "-" + Math.round(Math.random() * 1e9);
+    cb(null, `${uniqueSuffix}-${file.originalname}`);
   },
 });
 
-const upload = multer({ storage });
-
-// const upload = multer({ dest: "uploads/" });
+const upload = multer({ storage: storage });
 
 const app = express();
 const PORT = 3003;
@@ -24,16 +24,6 @@ app.get("/", (req, res) => {
   res.send("Hello from backend ");
 });
 
-// app.post("/uploads/pdf", upload.single("pdf"), (req, res) => {
-//   if (!req.pdf) {
-//     res.status(400).send("No file uploaded.");
-//     res.send(`File uploaded: ${req.pdf.filename}`);
-//   }
-//   //   res.send();
-// });
-// app.get("/upload", (req, res) => {
-//   res.status(200).send("uploaded file");
-// });
 app.post("/upload", upload.single("file"), (req, res) => {
   if (!req.file) return res.status(400).send("No file uploaded.");
   res.json({
