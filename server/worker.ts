921067c0f72cd2e4aefe9dab2c70e47e2c172c0f
Saima Sanjeeda Khan <119809514+saima-khan1@users.noreply.github.com>
2025-10-28 11:23:21 +0100

    add local embeddings with Hugging Face Transformers and Qdrant integration

--- server/worker.ts
+++ server/worker.ts
@@ -1,9 +1,13 @@
 import { Job, Worker } from "bullmq";
 import { QdrantVectorStore } from "@langchain/qdrant";
-import { OpenAIEmbeddings } from "@langchain/openai";
-import type { Document } from "@langchain/core/documents";
+import { Document } from "@langchain/core/documents";
 import { PDFLoader } from "@langchain/community/document_loaders/fs/pdf";
 import { CharacterTextSplitter } from "@langchain/textsplitters";
+import { pipeline } from "@xenova/transformers";
+import * as dotenv from "dotenv";
+import { HuggingFaceTransformersEmbeddings } from "@langchain/community/embeddings/huggingface_transformers";
+
+dotenv.config();
 const worker = new Worker(
   "file-upload-queue",
   async (job) => {
@@ -12,13 +16,37 @@ const worker = new Worker(
     const loader = new PDFLoader(data.path);
     const docs = await loader.load();
     const textSplitter = new CharacterTextSplitter({
-      chunkSize: 300,
+      chunkSize: 700,
       chunkOverlap: 0,
     });
+    const splitDocs: Document[] = [];
     for (const doc of docs) {
-      const texts = await textSplitter.splitText(doc.pageContent);
-      console.log(`Chunks for ${data.path}:`, texts);
+      const chunks = await textSplitter.splitText(doc.pageContent);
+      chunks.forEach((chunk) =>
+        splitDocs.push(
+          new Document({
+            pageContent: chunk,
+            metadata: { source: data.path },
+          })
+        )
+      );
     }
+    console.log(`✅ Created ${splitDocs.length} chunks`);
+
+    const model = new HuggingFaceTransformersEmbeddings({
+      model: "Xenova/all-MiniLM-L6-v2",
+    });
+    const vectorStore = await QdrantVectorStore.fromDocuments(
+      splitDocs,
+      model,
+      {
+        url: "http://localhost:6333",
+
+        collectionName: "pdf_vectors",
+      }
+    );
+    await vectorStore.addDocuments(splitDocs);
+    console.log(`✅ Stored embeddings in Qdrant`);
   },
   {
     connection: {
